593e50d44764931e5a579111433a3012b5da6e09
VictorMansilla <149539528+VictorMansilla@users.noreply.github.com>
2024-08-19 19:37:24 -0300

    Delete venv directory

--- venv/Lib/site-packages/paypalcheckoutsdk/__init__.py
+++
@@ -1,3 +0,0 @@
-name = "paypalcheckoutsdk"
-from paypalcheckoutsdk.config import *
-
--- venv/Lib/site-packages/paypalcheckoutsdk/core/util.py
+++
@@ -1,4 +0,0 @@
-
-def older_than_27():
-    import sys
-    return True if sys.version_info[:2] < (2, 7) else False
